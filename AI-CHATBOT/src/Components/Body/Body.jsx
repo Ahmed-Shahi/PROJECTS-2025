6d4eb758c3acdfc0ba2ed53799ebc13f68ef643e
Ahmed-Shahi <ahmedshahi038@gmail.com>
2025-02-15 16:36:01 +0500

    In Progress

--- AI-CHATBOT/src/Components/Body/Body.jsx
+++ AI-CHATBOT/src/Components/Body/Body.jsx
@@ -2,53 +2,54 @@ import { useState } from "react";
 import axios from 'axios';
 import './Body.css'
 import { FaPaperPlane, FaMicrophone } from "react-icons/fa";
-import result from "../../../index";
+
 const Body = () => {
 
- const [message, setMessage] = useState("");
-  const [talk, setTalk] = useState({})
+  const [message, setMessage] = useState("");
+  const [talk, setTalk] = useState([])
 
   const sendMessage = async () => {
-
-    setMessage("");
-    // const msg = message
     console.log("Loading...")
     try {
-    //   const response = await axios({
-    //     url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyBgyVfrkvJv-Jnqm7iSzkxStZImn20awq8",
-    //     method: "post",
-    //     // timeout: 10000,
-    //     data: {
-    //       "contents": [{
-    //         "parts": [{ "text": msg }]
-    //       }]
-    //     },
-    //   })      
-    //   const data = response?.data?.candidates?.[0]?.content?.parts?.[0]?.text || "No response received";
-      console.log(result);
-      
-      setTalk({
-        msg: `${message}`,
-        // ans: `${data}`,
+      const response = await axios({
+        url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyBgyVfrkvJv-Jnqm7iSzkxStZImn20awq8",
+        method: "post",
+        timeout: 10000,
+        data: {
+          "contents": [{
+            "parts": [{ "text": message }]
+          }]
+        },
       })
+      const data = response?.data?.candidates?.[0]?.content?.parts?.[0]?.text || "No response received";
+      if (message.trim() && data.trim()) {
+        setMessage("");
+        setTalk([...talk, {
+          msg: `${message}`,
+          ans: `${data}`,
+        }])
+
+      }
       console.log(talk)
     } catch (error) {
       console.error("Request timed out or failed:", error);
-         }
-
+    }
   };
+  
+  async function sendVoice() {
+    
 
+  }
   return (
     <div className="chatbot-container">
       <div className="chatbox">
-        {/* Chat Messages (Scrollable) */}
         <div className="messages">
-          {/* {allMessage.map((msg, index) => (
-            <div key={`msg-${index+1}`}>
-              <div className="message user">{msg}</div>
-              {allAns[index+1] && <div className="message bot">{allAns[index+1]}</div>}
+          {talk.map((data, index) => (
+            <div key={`${index}`}>
+              <div className="message user">{data.msg}</div>
+              <div className="message user">{data.ans}</div>
             </div>
-          ))} */}
+          ))}
         </div>
 
 
@@ -65,7 +66,7 @@ const Body = () => {
           <button className="send-btn" onClick={sendMessage}>
             <FaPaperPlane />
           </button>
-          <button className="voice-btn">
+          <button className="voice-btn" onClick={sendVoice}>
             <FaMicrophone />
           </button>
         </div>
@@ -74,6 +75,4 @@ const Body = () => {
   );
 };
 
-module.exports = {
-Body,
-}
+export default Body
